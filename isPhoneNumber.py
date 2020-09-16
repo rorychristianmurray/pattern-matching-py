40f536c4b099228aceb85fcba74274d2bde73856
unknown <rory@tacen.app>
2020-09-16 17:03:49 -0400

    Second examples

--- isPhoneNumber.py
+++ isPhoneNumber.py
@@ -1,29 +1,47 @@
-def isPhoneNumber(text):
-	if len(text) != 12:
-		return False
+import re
 
-	for i in range (0, 3):
-		if not text[i].isdecimal():
-			return False
+phoneNumRegex = re.compile(r'(\(\d{3}\))-(\d{3}-\d{4})')
 
-	if text[3] != '-':
-		return False
+mo = phoneNumRegex.search('this string has my num (503)-312-7679')
 
-	for i in range (4,7):
-		if not text[i].isdecimal():
-			return False
+print(f'phone number is {mo.group()}')
 
-	if text[7] != '-':
-		return False
+print(f'mo.group(1) : {mo.group(1)}')
 
-	for i in range(8,12):
-		if not text[i].isdecimal():
-			return False
+print(f'mo.group(2) : {mo.group(2)}')
 
-	return True
+print(f'mo.group(0) : {mo.group(0)}')
 
+print(f'mo.groups() : {mo.groups()}')
 
-print('Is 415-555-4242 a phone number?')
-print(isPhoneNumber('415-555-4242'))
-print('Is Moshi moshi a phone number?')
-print(isPhoneNumber('Moshi moshi'))
+areaCode, mainNumber = mo.groups()
+
+print(f'areaCode : {areaCode}')
+
+print(f'mainNumber : {mainNumber}')
+
+heroRegex = re.compile(r'Batman|Tina Fey')
+mo1 = heroRegex.search('Batman and Tina Fey')
+print(mo1.group())
+
+batRegex = re.compile(r'Bat(man|mobile|copter|bat)')
+
+bo = batRegex.search('The Batmobile lost a wheel!')
+print(bo.group())
+print(bo.group(1))
+
+batWoRegex = re.compile(r'Bat(wo)?man')
+
+bw = batWoRegex.search('Batman is here')
+bw1 = batWoRegex.search('Batwoman is here')
+
+print(f'bw : {bw.group()} bw1 : {bw1.group()}')
+
+phoneRegOpt = re.compile(r'(\d{3}-)?\d{3}-\d{4}')
+
+po = phoneRegOpt.search('check out 50-312-7679')
+print(f'po : {po.group()}')
+
+phoneRegFA = re.compile(r'(\d{3})-(\d{3})-(\d{4})')
+fao = phoneRegFA.findall('c: 503-312-7679 o: 897-990-6789')
+print(f'fao : {fao}')
